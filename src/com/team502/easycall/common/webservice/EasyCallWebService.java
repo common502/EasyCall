29bb1dc5b9194b8d29bfa8166aa0772312b0b5d1
Antion502 <363166254@qq.com>
2013-12-27 16:12:35 +0800

    Signed-off-by: Antion502 <363166254@qq.com>

---
+++ src/com/team502/easycall/common/webservice/EasyCallWebService.java
@@ -0,0 +1,15 @@
+/*
+ * @(#)EasyCallWebService.java 2013年12月27日
+ * Copyright (c), 2013 深圳业拓讯通信科技有限公司（Shenzhen Yetelcom Communication Tech. Co.,Ltd.）,  
+ * 著作权人保留一切权利，任何使用需经授权。
+ */
+package com.team502.easycall.common.webservice;
+/** 
+ *  
+ * @author Antion
+ * @since
+ * @version 
+ */
+public class EasyCallWebService {
+
+}
